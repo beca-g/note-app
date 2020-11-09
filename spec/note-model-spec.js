d8bba99c3271110940d31306f9b1fe06c65cb06b
Beca-g <rebekahgalliano@gmail.com>
2020-11-09 17:15:41 +0000

    Refactored test to use an assertion. Added assert.js to lib folder.

--- spec/note-model-spec.js
+++ spec/note-model-spec.js
@@ -1,12 +1,20 @@
 "use strict";
 
-(function(exports) {
-  function testTextInstantiation() {
-    let note = new Note('My favourite language is JavaScript');
+function textIsNotEmpty() {
+  let note = new Note("I hate JS");
+  assert.isTrue(note.text === "I hate JS");
+};
 
-    if (note.text !== 'My favourite language is JavaScript') {
-      throw new Error(`Expected 'My favourite language is JavaScript' but got ${note.text}`)
-    }
-  };
-  testTextInstantiation();
-})(this);
+textIsNotEmpty();
+
+
+// (function(exports) {
+//   function testTextInstantiation() {
+//     let note = new Note('My favourite language is JavaScript');
+
+//     if (note.text !== 'My favourite language is JavaScript') {
+//       throw new Error(`Expected 'My favourite language is JavaScript' but got ${note.text}`)
+//     }
+//   };
+//   testTextInstantiation();
+// })(this);
