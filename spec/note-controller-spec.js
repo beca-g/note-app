30328b9157e6fba58386b53f9baf80252aee8960
Beca-g <rebekahgalliano@gmail.com>
2020-11-13 13:40:33 +0000

    Updated note controller with links

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -10,7 +10,7 @@
   function canChangeInnerHtml() { 
     appDiv = document.getElementById("app");
     noteController.insertHtml();
-    assert.isTrue(appDiv.innerHTML === "<ul><li><div>Favourite drink: sel</div></li></ul>");
+    assert.isTrue(appDiv.innerHTML === '<ul><li><div><a id="0" href="#note/0">Favourite drink: sel</a></div></li></ul>');
     console.log("Test passed: NoteController can updated the innerHTML of the app element")
   }
   canChangeInnerHtml();
